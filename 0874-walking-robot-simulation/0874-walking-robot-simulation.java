85ccec56eb628441bb76085e24952195f85814a7
Rahul Jeena <151987448+JrRahul18@users.noreply.github.com>
2024-09-04 12:00:36 +0530

    Time: 35 ms (48.91%), Space: 48.9 MB (85.59%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.java
@@ -0,0 +1,37 @@
+class Solution {
+    static int[] xArray = {0, 1, 0, -1};
+    static int[] yArray = {1, 0, -1, 0};
+    public int robotSim(int[] commands, int[][] obstacles) {
+        int finalAns = 0;
+        
+        int x=0, y=0, d=0;
+        
+        Set<String> hset = new HashSet<>();
+        for(int[] obs: obstacles){
+            hset.add(obs[0] + " " + obs[1]);
+        }
+        
+        for(int c: commands){
+            if(c == -1){
+                d = (d+1)%4;
+            }
+            else if(c == -2){
+                d = (d+3)%4;
+            }
+            else{
+                for(int i=1; i<=c; i++){
+                    if(!hset.contains( (x+xArray[d]) + " " + (y+yArray[d]) )){
+                        x=x+xArray[d];
+                        y=y+yArray[d];
+                    }
+                    else{
+                        break;
+                    }
+                }
+                
+                finalAns = Math.max(finalAns, x*x + y*y);
+            }
+        }
+        return finalAns;
+    }
+}
