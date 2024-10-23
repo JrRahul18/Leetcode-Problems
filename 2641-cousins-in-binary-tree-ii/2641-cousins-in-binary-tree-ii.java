a7bc9f1cadfb99ad75e66ec0e9ffbfa86f178282
Rahul Jeena <151987448+JrRahul18@users.noreply.github.com>
2024-10-23 23:49:04 +0530

    Time: 22 ms (66.95%), Space: 76.8 MB (73.73%) - LeetHub

--- 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.java
@@ -16,47 +16,51 @@
 class Solution {
     public TreeNode replaceValueInTree(TreeNode root) {
         if (root == null) return root;
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
-        List<Integer> levelSum = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        List<Integer> lst = new ArrayList<>();
         
-         while (!queue.isEmpty()) {
-            int currLevelSum = 0;
-            int n = queue.size();
+         while (!q.isEmpty()) {
+            int current = 0;
+            int n = q.size();
             for (int i = 0; i < n; i++) {
-                TreeNode curr = queue.poll();
-                currLevelSum += curr.val;
-                if (curr.left != null) {
-                    queue.add(curr.left);
+                TreeNode temp = q.peek();
+                q.poll();
+                current += temp.val;
+                if (temp.left != null) {
+                    q.add(temp.left);
                 }
-                if (curr.right != null) {
-                    queue.add(curr.right);
+                if (temp.right != null) {
+                    q.add(temp.right);
                 }
             }
-            levelSum.add(currLevelSum);
+            lst.add(current);
         }
         
-        queue.add(root);
+        q.add(root);
         root.val = 0;  // root has no cousins
         int level = 1;
-        while (!queue.isEmpty()) {
-            int n = queue.size();
+        while (!q.isEmpty()) {
+            int n = q.size();
 
             for (int i = 0; i < n; i++) {
-                TreeNode curr = queue.poll();
-
-                // levelSum of currentLevel - siblingSum
-                int siblingSum = (curr.left != null ? curr.left.val : 0) + (curr.right != null ? curr.right.val : 0);
-
-                if (curr.left != null) {
-                    curr.left.val = levelSum.get(level) - siblingSum;
-                    queue.add(curr.left);
+                TreeNode temp = q.peek();
+                q.poll();
+                int childSum = 0;
+                if(temp.left != null) childSum+=temp.left.val;
+                if(temp.right != null) childSum+=temp.right.val;
+                
+                if(temp.left != null){
+                    temp.left.val = lst.get(level) - childSum;
+                    q.add(temp.left);
                 }
-                if (curr.right != null) {
-                    curr.right.val = levelSum.get(level) - siblingSum;
-                    queue.add(curr.right);
+                
+                if(temp.right != null){
+                    temp.right.val = lst.get(level) - childSum;
+                    q.add(temp.right);
                 }
             }
+            
             level++;
         }
 
