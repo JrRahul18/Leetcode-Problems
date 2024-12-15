a88f358912d1952214fd8e97eee6d4cbfd53ec68
Rahul Jeena <151987448+JrRahul18@users.noreply.github.com>
2024-12-15 12:52:35 +0530

    Time: 1312 ms (5.07%), Space: 103.8 MB (14.63%) - LeetHub

---
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.java
@@ -0,0 +1,55 @@
+class Solution {
+    class Tuple{
+        double passed;
+        double total;
+        double passDiff;
+        
+        Tuple(double passed, double total, double passDiff){
+            this.passed = passed;
+            this.total = total;
+            this.passDiff = passDiff;
+        }
+        
+    }
+    public double maxAverageRatio(int[][] classes, int extraStudents) {
+        double finalAns=0;
+        PriorityQueue<Tuple> pq = new PriorityQueue<>( (a, b) -> Double.compare(b.passDiff, a.passDiff) );
+        
+        for(int[] c: classes){
+            double c0 = (double)c[0];
+            double c1 = (double)c[1];
+            
+            double currentPassed = (double) (c0/c1);
+            double newPassed = (double) ((c0+1)/(c1+1));
+            
+            double newPassRatio = ((c0+1)/(c1+1)) - (c0/c1) ;
+            System.out.println(newPassRatio);
+            pq.add(new Tuple((double) c[0], (double) c[1], newPassRatio));
+        }
+        
+        while(extraStudents > 0){
+            Tuple temp = pq.peek();
+            pq.poll();
+            
+            temp.passed+=1;
+            temp.total+=1;
+            
+            double currentPassed = (double) (temp.passed/temp.total);
+            double newPassed = (double) ((temp.passed+1)/(temp.total+1));
+            
+            double newPassRatio  = newPassed-currentPassed;
+            
+            pq.add(new Tuple(temp.passed, temp.total, newPassRatio));
+            extraStudents--;
+        }
+        
+        while(!pq.isEmpty()){
+            Tuple temp = pq.peek();
+            pq.poll();
+            System.out.println( temp.passDiff +" "+ temp.passed + " " + temp.total);
+            finalAns += temp.passed/temp.total;
+        }
+        return finalAns/classes.length;
+        
+    }
+}
